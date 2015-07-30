ac8d9dec84309b4ee77513edd5d3733a354bfe75
cjmanetta & ryanau <pair+cjmanetta.ryanau@devbootcamp.com>
2015-07-30 12:02:35 -0700

    finish CRUD with no ajax hahaha

--- classic_todolist/app/controllers/items_controller.rb
+++ classic_todolist/app/controllers/items_controller.rb
@@ -13,6 +13,13 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      @error = "Item didn't save"
+      render 'new'
+    end
   end
 
   def edit
@@ -20,9 +27,20 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      @error = "Item didn't update"
+      render 'edit'
+    end
   end
 
   def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
   end
 
   private
